5f74559e1e54e9e30fe404ffd9cc4500ab95d72d
sweeetcc <sweeetcc@gmail.com>
2017-01-17 14:55:06 +0800

    feat: modify generator

---
+++ app/client/containers/NotFound/index.js
@@ -0,0 +1,45 @@
+/**
+ * NotFound
+ */
+
+/**
+ * External dependencies
+ */
+import React, { PropTypes, PureComponent } from 'react';
+import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
+
+/**
+ * Internal dependencies
+ */
+import './style.less';
+
+
+export class NotFound extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="NotFound"
+          meta={[
+            { name: 'description', content: 'Description of NotFound' },
+          ]}
+        />
+        NotFound
+      </div>
+    );
+  }
+}
+
+NotFound.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(null, mapDispatchToProps)(NotFound);
