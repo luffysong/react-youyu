c85b5d2baffed03c3c8928a5c6d66042eb1715cd
sweeetcc <sweeetcc@gmail.com>
2017-02-04 16:46:45 +0800

    feat: 关于我们页面

--- app/client/containers/About/index.js
+++ app/client/containers/About/index.js
@@ -19,12 +19,39 @@ export class About extends PureComponent {
     return (
       <div className="about-container">
         <Helmet
-          title="About"
+          title="关于我们"
           meta={[
-            { name: 'description', content: 'Description of About' },
+            { name: 'description', content: '关于我们' },
           ]}
         />
-        About
+        <div className="about-wrapper container">
+          <h1 className="about-title">关于我们</h1>
+          <div className="about-desc">
+            有娱是36氪金融与普思资本（万达集团旗下）共同成立的国内首家影视类项目投资收益权转让平台，依托天津金融资产交易所，提供规范和专业的文娱类项目可转让投资收益权交易服务。
+          </div>
+          <div className="about-parts">
+            <div className="about-parts-product">
+              <img className="about-parts-img" src={require('./imgs/icon_ourproduct_about.svg')} alt="我们的产品" />
+              <h2 className="about-parts-title">我们的产品</h2>
+              <ul className="about-parts-list">
+                <li className="about-parts-list-item">电影投资收益权转让</li>
+                <li className="about-parts-list-item">电视剧应收账款收益权转让</li>
+                <li className="about-parts-list-item">影城项目投资收益权转让</li>
+              </ul>
+            </div>
+            <div className="about-parts-split"></div>
+            <div className="about-parts-product">
+              <img className="about-parts-img" src={require('./imgs/icon_service_about.svg')} alt="我们的服务" />
+              <h2 className="about-parts-title">我们的服务</h2>
+              <ul className="about-parts-list">
+                <li className="about-parts-list-item">严格的交易风险控制流程</li>
+                <li className="about-parts-list-item">合格的金融资产交易所资质</li>
+                <li className="about-parts-list-item">优质的投资标的筛选</li>
+                <li className="about-parts-list-item">规范的投资收益分配规则</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
