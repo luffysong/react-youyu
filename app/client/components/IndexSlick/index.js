b38d3fcf59be2140c1f64ab162c88f80f763b303
zhouxiao <zhouxiao@36kr.com>
2017-02-08 11:43:49 +0800

    feat: 首页轮播图开发

---
+++ app/client/components/IndexSlick/index.js
@@ -0,0 +1,58 @@
+/**
+ * Slick
+ */
+
+/**
+ * External dependencies
+ */
+import React, { PureComponent } from 'react';
+import Slider from 'react-slick';
+import classnames from 'classnames';
+import $ from 'jquery';
+
+/**
+ * Internal dependencies
+ */
+import './style.less';
+
+class IndexSlick extends PureComponent {
+  componentDidMount() {
+
+    /*const classList = ['carousel-item-first', 'carousel-item', 'carousel-item-last'];
+    $(this.refs.child).children().each((index, item) => item.className = classList[index]);*/
+    this.timer = window.setInterval(() => {
+      let arr = [];
+      const len = $(this.refs.child).children().length;
+      $(this.refs.child).children().each((index, item) => arr.push(item.className));
+      $(this.refs.child).children().each((index, item) => {
+        var temp;
+        if (`${index}` === `0`) {
+          temp = len - 1;
+        } else {
+          temp = index - 1;
+        }
+        item.className = arr[temp];
+      });
+    }, 2000);
+
+  }
+
+  componentWillUnmount() {
+    /*组件销毁时,清除定时器*/
+    window.clearInterval(this.timer);
+  }
+  render() {
+
+    return (
+      <div className="index-slick-wrapper" ref="child">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+IndexSlick.propTypes = {
+  /*className: React.PropTypes.string.isRequired,*/
+};
+
+export default IndexSlick;
--- app/client/components/ProjectCarousel/index.js
+++ app/client/components/ProjectCarousel/index.js
@@ -1,4 +1,5 @@
 /**
+ *
  * ProjectCarousel
  */
 
@@ -12,8 +13,10 @@ import React, { PureComponent } from 'react';
  */
 import './style.less';
 import ProjectInfoBar from '../ProjectInfoBar';
+import IndexSlick from '../IndexSlick';
 
 class ProjectCarousel extends PureComponent {
+
   render() {
     const data = [
       {
@@ -34,62 +37,68 @@ class ProjectCarousel extends PureComponent {
       <div className="project-carousel-component">
         <div className="carousel-title">转让中的项目</div>
         <div className="carousel-wrapper">
-          <div className="carousel-item clearfix">
-            <div className="cover"></div>
-            <div className="info">
-              <div className="info-title">
-                神奇动物在哪里
-              </div>
-              <ProjectInfoBar data={data} />
-              <table className="transfer-info">
-                <tbody>
-                  <tr className="transfer-info-item">
-                    <td className="item-info">
-                      <span>转让价格：</span>
-                      <span className="font-white">￥10,000元</span>
-                    </td>
-                    <td>
-                      <span>转让份额：</span>
-                      <span className="font-white">1%</span>
-                    </td>
-                    <td>
-                      <span>转让方：</span>
-                      <span>章小勇</span>
-                    </td>
-                  </tr>
-                  <tr className="transfer-info-item">
-                    <td className="item-info">
-                      <span>转让价格：</span>
-                      <span className="font-white">￥10,000元</span>
-                    </td>
-                    <td>
-                      <span>转让份额：</span>
-                      <span className="font-white">1%</span>
-                    </td>
-                    <td>
-                      <span>转让方：</span>
-                      <span>章小勇</span>
-                    </td>
-                  </tr>
-                  <tr className="transfer-info-item">
-                    <td className="item-info">
-                      <span>转让价格：</span>
-                      <span className="font-white">￥10,000元</span>
-                    </td>
-                    <td>
-                      <span>转让份额：</span>
-                      <span className="font-white">1%</span>
-                    </td>
-                    <td>
-                      <span>转让方：</span>
-                      <span>章小勇</span>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <a className="more-link" href="">还有5个转让...</a>
-            </div>
-          </div>
+          <IndexSlick>
+            {
+              ['carousel-item-first', 'carousel-item', 'carousel-item-last'].map((item, index) => {
+                return <div className={`${item}`} key={index}>
+                  <div className="cover"></div>
+                  <div className="info">
+                    <div className="info-title">
+                      神奇动物在哪里
+                    </div>
+                    <ProjectInfoBar data={data} />
+                    <table className="transfer-info">
+                      <tbody>
+                      <tr className="transfer-info-item">
+                        <td className="item-info">
+                          <span>转让价格：</span>
+                          <span className="font-white">￥10,000元</span>
+                        </td>
+                        <td>
+                          <span>转让份额：</span>
+                          <span className="font-white">1%</span>
+                        </td>
+                        <td>
+                          <span>转让方：</span>
+                          <span>章小勇</span>
+                        </td>
+                      </tr>
+                      <tr className="transfer-info-item">
+                        <td className="item-info">
+                          <span>转让价格：</span>
+                          <span className="font-white">￥10,000元</span>
+                        </td>
+                        <td>
+                          <span>转让份额：</span>
+                          <span className="font-white">1%</span>
+                        </td>
+                        <td>
+                          <span>转让方：</span>
+                          <span>章小勇</span>
+                        </td>
+                      </tr>
+                      <tr className="transfer-info-item">
+                        <td className="item-info">
+                          <span>转让价格：</span>
+                          <span className="font-white">￥10,000元</span>
+                        </td>
+                        <td>
+                          <span>转让份额：</span>
+                          <span className="font-white">1%</span>
+                        </td>
+                        <td>
+                          <span>转让方：</span>
+                          <span>章小勇</span>
+                        </td>
+                      </tr>
+                      </tbody>
+                    </table>
+                    <a className="more-link" href="">还有5个转让...</a>
+                  </div>
+                </div>
+              })
+            }
+          </IndexSlick>
         </div>
       </div>
     );
