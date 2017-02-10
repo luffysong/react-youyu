e895e1fbb7b6a8a8334f0a0b0da9c4dca533bc52
sweeetcc <sweeetcc@gmail.com>
2017-02-10 18:04:32 +0800

    feat: 新闻公告

--- app/client/containers/NewsDetail/reducer.js
+++ app/client/containers/NewsDetail/reducer.js
@@ -10,16 +10,27 @@ import { fromJS } from 'immutable';
 /**
  * Internal dependencies
  */
-import {
-  DEFAULT_ACTION,
-} from './constants';
+import * as types from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  newsDetailLoading: {},
+  newsDetailData: {},
+  newsDetailError: {},
+});
 
 function newsDetailReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case types.LOAD_NEWS_DETAIL:
+      return state
+        .setIn(['newsDetailLoading', action.id], true);
+    case types.LOAD_NEWS_DETAIL_SUC:
+      return state
+        .setIn(['newsDetailLoading', action.id], false)
+        .setIn(['newsDetailData', action.id], action.data.info);
+    case types.LOAD_NEWS_DETAIL_ERR:
+      return state
+        .setIn(['newsDetailLoading', action.id], false)
+        .setIn(['newsDetailError', action.id], action.err);
     default:
       return state;
   }
--- app/client/utils/utils.js
+++ app/client/utils/utils.js
@@ -1,3 +1,8 @@
 export function numComma(x, prefix, suffix) {
   return (prefix ? '￥' : '') + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (suffix ? '元' : '');
 }
+
+export function removeInlineStyle(str) {
+  const REGEX = /(?:\G(?!^)|\bstyle=")([^:]*):\s*([^;]*)[;"](?=[^>]*>)/g;
+  return str.replace(REGEX, '');
+}
