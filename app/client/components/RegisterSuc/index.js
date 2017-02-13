e51f983e2eedd7f492c9c3afaaf944bb30f038e7
zhouxiao <zhouxiao@36kr.com>
2017-02-13 18:43:37 +0800

    feat: 企业注册上传组件接入

---
+++ app/client/components/RegisterSuc/index.js
@@ -0,0 +1,53 @@
+/**
+ * QuoteSuc
+ */
+
+/**
+ * External dependencies
+ */
+import React, {PureComponent} from 'react';
+import { Link } from 'react-router';
+
+/**
+ * Internal dependencies
+ */
+import './style.less';
+
+class RegisterSuc extends PureComponent {
+  render() {
+    return (
+      <div className="register-suc text-center">
+        <div className="dis-in-block">
+          <img className="suc-logo" src={require('./imgs/submitSuc.png')} alt=""/>
+          <section className="submit-suc">提交完成</section>
+          <section className="suc-tip">请您请于7个工作日将贵单位如下资质邮寄至我公司，我们将在收到您合格资质后的1个工作日进行审核。</section>
+          <section className="list-section">
+            <section className="list-title">资质提供 :</section>
+            <section className="list-content">
+              1、公司章程或合伙协议、授权委托书；
+            </section>
+            <section className="list-content">
+              2、企业法人营业执照副本复印件、组织机构代码证复印件、税务登记证复印件；
+            </section>
+            <section className="list-content">
+              3、法定代表人身份证明及身份证复印件；
+            </section>
+            <section className="list-tip">
+              以上材料中均需加盖本单位公章的纸质文件，并注明“本企业（或组织）对提交资料的真实性负法律责任”。
+            </section>
+          </section>
+          <section className="list-section">
+            <section className="list-title">邮寄地址 :</section>
+            <section className="list-content">
+              北京市海淀区海淀大街34号海置创投大厦5层  xxx收
+            </section>
+          </section>
+        </div>
+
+        <Link to={`/quote/initial/${this.props.id}/3`} activeClassName="active" className="next-btn">了解下平台交易细则</Link>
+      </div>
+    );
+  }
+}
+
+export default RegisterSuc;
--- app/client/containers/Register/Company.js
+++ app/client/containers/Register/Company.js
@@ -8,12 +8,17 @@
 import React, { PropTypes, PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import message from '../../components/Message';
 
 /**
  * Internal dependencies
  */
 import './style.less';
 import * as actions from './actions';
+import UploadBtn from  '../../components/UploadButton';
+import RegisterSuc from '../../components/RegisterSuc';
+
+
 
 export class Company extends PureComponent {
   constructor(props) {
@@ -21,8 +26,43 @@ export class Company extends PureComponent {
 
     this.state = {
       memberType: '1',
-      agree: true
+      agree: true,
+      submitSuc: false
     };
+
+    this.uploadParams = {
+      success(data) {
+        this.setState({
+          uploadedImg: data
+        });
+        this.setState({
+          uploading: false,
+          uploaded: true
+        });
+        console.log(data, 'suc');
+      },
+      progress(data) {
+        this.setState({
+          uploading: true,
+          uploadProgress: `${data}%`
+        });
+        console.log(data, 'pro');
+      },
+      error(msg) {
+        this.setState({
+          uploading: false,
+        });
+        console.log(msg);
+        message.error(msg);
+      },
+      params: {
+        'image-width-range': '0, 3000'
+      }
+    };
+
+    this.uploadParams.success = this.uploadParams.success.bind(this);
+    this.uploadParams.progress = this.uploadParams.progress.bind(this);
+    this.uploadParams.error = this.uploadParams.error.bind(this);
   }
 
   selectType(event) {
@@ -41,83 +81,97 @@ export class Company extends PureComponent {
     this.props.orgRegister({
        name: this.refs.orgName.value || '',
        code: this.refs.orgCode.value || '',
-       license_pic: '',
+       license_pic: this.state.uploadedImg,
        type: this.state.memberType
-    }).then(data => console.log(data))
+    }).then(data => this.setState({submitSuc: true}))
       .catch(err => console.log(err));
   }
 
   render() {
     return (
-      <div className="register-company-container">
-        <h5 className="register-title">企业注册</h5>
-        <div className="list-col">
-          <div className="col-attr">
-            企业全称
-          </div>
-          <div className="col-value">
-            <input type="text" className="price-input" ref='orgName' />
-          </div>
-        </div>
-        <div className="list-col">
-          <div className="col-attr">
-            社会信用代码
-          </div>
-          <div className="col-value">
-            <input type="text" className="price-input" ref="orgCode" />
-          </div>
-        </div>
-
-        <div className="list-col">
-          <div className="col-attr">
-            营业执照
-          </div>
-          <div className="col-value">
-            <span className="upload-btn">
-              点击上传
-              <input type="file"/>
-            </span>
-          </div>
-        </div>
-
-        <div className="list-col">
-          <div className="col-attr">
-            会员类型
-          </div>
-          <div className="col-value member-type">
-            <section>
-              <div className={this.state.memberType === '1' ? 'quote-radio checked' : 'quote-radio'}>
-                <input type="radio" checked={this.state.memberType === '1'} name="memberType" onChange={this.selectType.bind(this)} value="1" id="business" />
+      <div>
+        {
+          this.state.submitSuc ? <RegisterSuc /> :
+            <div className="register-company-container">
+              <h5 className="register-title">企业注册</h5>
+              <div className="list-col">
+                <div className="col-attr">
+                  企业全称
+                </div>
+                <div className="col-value">
+                  <input type="text" className="price-input" ref='orgName' />
+                </div>
               </div>
-              <label htmlFor="business">
-                交易会员(普通投资会员)
-              </label>
-            </section>
-            <section>
-              <div className={this.state.memberType === '2' ? 'quote-radio checked' : 'quote-radio'}>
-                <input type="radio" checked={this.state.memberType === '2'} name="memberType" onChange={this.selectType.bind(this)} value="2" id="composite" />
+              <div className="list-col">
+                <div className="col-attr">
+                  社会信用代码
+                </div>
+                <div className="col-value">
+                  <input type="text" className="price-input" ref="orgCode" />
+                </div>
               </div>
-              <label htmlFor="composite">
-                综合会员(持有影视初始份额的会员)
-              </label>
-            </section>
-          </div>
-        </div>
 
-        <div className="list-col">
-          <div className="col-attr">
-          </div>
-          <div className="col-value">
-            <div className={this.state.agree ? 'quote-radio checked' : 'quote-radio'}>
-              <input type="radio" name="memberType" onChange={this.agree.bind(this)} value="agree" id="agree" />
+              <div className="list-col">
+                <div className={`col-attr ${this.state.uploading || this.state.uploaded ? 'ver-top' : ''}`}>
+                  营业执照
+                </div>
+                <div className={`col-value ${this.state.uploading ? 'uploading' : ''} ${this.state.uploadedImg ? 'uploaded' : ''}`}>
+                  {
+                    this.state.uploading || this.state.uploaded ?
+                      <div className="uploaded-pic" style={this.state.uploadedImg ? {backgroundImage: `url(${this.state.uploadedImg})`} : {}}>
+                        {
+                          this.state.uploading ? <div className="upload-progress" style={{height: this.state.uploadProgress}}></div> : null
+                        }
+                      </div> : null
+                  }
+                  <UploadBtn {...this.uploadParams}>
+                    {
+                      this.state.uploadedImg ? '重新上传' : '点击上传'
+                    }
+                  </UploadBtn>
+                </div>
+              </div>
+
+              <div className="list-col">
+                <div className="col-attr">
+                  会员类型
+                </div>
+                <div className="col-value member-type">
+                  <section>
+                    <div className={this.state.memberType === '1' ? 'quote-radio checked' : 'quote-radio'}>
+                      <input type="radio" checked={this.state.memberType === '1'} name="memberType" onChange={this.selectType.bind(this)} value="1" id="business" />
+                    </div>
+                    <label htmlFor="business">
+                      交易会员(普通投资会员)
+                    </label>
+                  </section>
+                  <section>
+                    <div className={this.state.memberType === '2' ? 'quote-radio checked' : 'quote-radio'}>
+                      <input type="radio" checked={this.state.memberType === '2'} name="memberType" onChange={this.selectType.bind(this)} value="2" id="composite" />
+                    </div>
+                    <label htmlFor="composite">
+                      综合会员(持有影视初始份额的会员)
+                    </label>
+                  </section>
+                </div>
+              </div>
+
+              <div className="list-col">
+                <div className="col-attr">
+                </div>
+                <div className="col-value">
+                  <div className={this.state.agree ? 'quote-radio checked' : 'quote-radio'}>
+                    <input type="radio" name="memberType" onChange={this.agree.bind(this)} value="agree" id="agree" />
+                  </div>
+                  <label htmlFor="agree">
+                    同意《会员合同》《XXXX协议》
+                  </label>
+                </div>
+              </div>
+
+              <Link to="" activeClassName="active" className={`next-btn ${this.state.agree ? '' : 'disabled'}`} onClick={this.submit.bind(this)}>下一步</Link>
             </div>
-            <label htmlFor="agree">
-              同意《会员合同》《XXXX协议》
-            </label>
-          </div>
-        </div>
-
-        <Link to="" activeClassName="active" className={`next-btn ${this.state.agree ? '' : 'disabled'}`} onClick={this.submit.bind(this)}>下一步</Link>
+        }
       </div>
     );
   }
