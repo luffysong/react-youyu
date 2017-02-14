1e2a1e104fda425d5775e7d7f6a87c745679d28a
小包 <luxinyue@36kr.com>
2017-02-14 10:37:14 +0800

    chore: 部署脚本

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -5,4 +5,4 @@ npm run build:client:$2
 
 path="/data/docker/$2/work/frontend/prod/36jr/youyu/dist"
 ssh ali-rong-$1 "mkdir -p $path"
-rsync -rvltOD ./build/* "www@ali-rong-$1:$path"
+rsync -rvltOD ./build/client/* "www@ali-rong-$1:$path"
