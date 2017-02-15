9f1994afebd3eaa0a35b945eba3a7a8eb2520cde
zhouxiao <zhouxiao@36kr.com>
2017-02-15 15:07:16 +0800

    feat: 404页面开发

--- app/client/containers/NotFound/index.js
+++ app/client/containers/NotFound/index.js
@@ -8,6 +8,7 @@
 import React, { PropTypes, PureComponent } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
+import { Link } from 'react-router';
 
 /**
  * Internal dependencies
@@ -25,7 +26,20 @@ export class NotFound extends PureComponent {
             { name: 'description', content: 'Description of NotFound' },
           ]}
         />
-        NotFound
+        <div className="not-found-content">
+          <img className="not-found-logo" src={require('./imgs/not-found-logo.png')} alt="404" width='384' />
+          <img className="shadow" src={require('./imgs/shadow.png')} alt="404" width='356' />
+          <img className="grey-ball" src={require('./imgs/grey-ball.png')} width='42' alt="" />
+          <img className="blue-ball" src={require('./imgs/blue-ball.png')} width='54' alt="" />
+          <img className="green-ball" src={require('./imgs/green-ball.png')} width='31' alt="" />
+          <img className="orange-ball" src={require('./imgs/orange-ball.png')} width='42' alt="" />
+          <img className="purple-ball" src={require('./imgs/purple-ball.png')} width='42' alt="" />
+        </div>
+        <section className="not-found-text">
+          糟糕，您查询的页面找不到了，首页还有更多精彩内容哦！
+        </section>
+        <Link to={`/`} activeClassName="active" className="index-btn">返回首页</Link>
+
       </div>
     );
   }
