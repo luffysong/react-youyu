0b4ee0258443a11a46d9e0bc1f9cc5aa29acb4ec
sweeetcc <sweeetcc@gmail.com>
2017-02-15 14:31:10 +0800

    feat: 订单分页

--- app/client/containers/OrderMgmt/index.js
+++ app/client/containers/OrderMgmt/index.js
@@ -22,7 +22,11 @@ import * as actions from './actions';
 export class OrderMgmt extends PureComponent {
   constructor(props) {
     super(props);
+    const query = this.props.location.query;
+    const status = this.props.params.status;
     this.state = {
+      page: query.page ? parseInt(query.page, 10) : 1,
+      status,
       STATUS: {
         open: '待付款',
         succeed: '已完成',
@@ -30,19 +34,41 @@ export class OrderMgmt extends PureComponent {
         all: '全部',
       },
     };
+    this.onPageChange = this.handlePageChange.bind(this);
   }
 
   componentDidMount() {
-    const status = this.props.params.status;
-    const query = this.props.location.query;
-    const page = query.page ? query.page : 1;
-    if (!this.props.orderListData) {
-      this.props.getOrderList(status, page);
+    this.props.getOrderList(this.state.status, this.state.page);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const page = this.state.page;
+    const oldPage = prevState.page;
+
+    if (page !== oldPage) {
+      this.props.getOrderList(this.state.status, page);
     }
+  }
+
+  handlePageChange(page) {
+    if (!page || page.selected === undefined) {
+      return false;
+    }
+    this.props.router.push({
+      pathname: `/uc/orderMgmt/${this.state.status}`,
+      query: {
+        page: parseInt(page.selected + 1, 10),
+      }
+    });
   }
 
   render() {
     const navLinks = [];
+    const { orderListData, orderListLoading } = this.props;
+    const pageInfo = {
+      currentPage: get(orderListData, 'current_page'),
+      lastPage: get(orderListData, 'last_page'),
+    };
 
     for (let item in this.state.STATUS) {
       if (this.state.STATUS.hasOwnProperty(item)) {
@@ -69,7 +95,11 @@ export class OrderMgmt extends PureComponent {
             })
           }
         </div>
-        <Pagination className="order-mgmt-pagination" />
+        {
+          orderListLoading
+          ? null
+          : <Pagination pageInfo={pageInfo} onPageChange={this.onPageChange} className="order-mgmt-pagination" />
+        }
       </div>
     );
   }
@@ -83,7 +113,7 @@ function mapStateToProps(state, props) {
   const orderMgmt = state.orderMgmt;
   const status = props.params.status;
   const query = props.location.query;
-  const page = query.page ? query.page : 1;
+  const page = query.page ? parseInt(query.page, 10) : 1;
 
   return {
     orderListData: orderMgmt.getIn(['orderListData', status, page]),
