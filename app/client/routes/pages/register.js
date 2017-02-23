41862896c1950c4a3891cc76f81f902caa61ffa3
小包 <luxinyue@36kr.com>
2017-02-23 09:48:09 +0800

    fix: 用户有身份时，认证页跳转地址

--- app/client/routes/pages/register.js
+++ app/client/routes/pages/register.js
@@ -46,9 +46,11 @@ export default function registerRoute(loadModule, injectReducer) {
             const identityType = get(infoCache, 'userInfo.info.operation_steps.identity_type');
 
             if(!memberType && memberStatus === 1 && identityType === 1) {
-              replace('/register/personalresult')
+              replace('/register/personalresult');
             } else if(!memberType && memberStatus === 1 && identityType === 2) {
-              replace('/register/companyresult')
+              replace('/register/companyresult');
+            } else if (memberType) {
+              replace('/projects');
             }
             callback();
           }
@@ -77,9 +79,11 @@ export default function registerRoute(loadModule, injectReducer) {
             const memberStatus = get(infoCache, 'userInfo.info.operation_steps.member_status');
             const identityType = get(infoCache, 'userInfo.info.operation_steps.identity_type');
             if(!memberType && memberStatus === 1 && identityType === 1) {
-              replace('/register/personalresult')
+              replace('/register/personalresult');
             } else if(!memberType && memberStatus === 1 && identityType === 2) {
-              replace('/register/companyresult')
+              replace('/register/companyresult');
+            } else if (memberType) {
+              replace('/projects');
             }
             callback();
           }
