5e3b19c10adef2319b8aafa101d030f5858b819e
sweeetcc <sweeetcc@gmail.com>
2017-03-01 19:39:19 +0800

    fix: 修改 gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@
  * External dependencies
  */
 const gulp = require('gulp');
-const webpack = require('webpack');
-const gutil = require('gulp-util');
 const nodemon = require('gulp-nodemon');
 const spawn = require('child_process').spawn;
 const axios = require('axios');
@@ -14,28 +12,17 @@ const _ = require('lodash');
 /**
  * Internal dependencies
  */
-const serverProductionConfig = require('./internals/webpack/server/production');
-const serverDevelopmentConfig = require('./internals/webpack/server/development');
 
-gulp.task('webpack:production', (done) => webpackProcess(done, serverProductionConfig));
-
-gulp.task('webpack:development', (done) => webpackProcess(done, serverDevelopmentConfig));
-
-gulp.task('server:build', ['webpack:production']);
-
-gulp.task('server:watch', ['webpack:development'], () => {
-  const env = process.env.NODE_ENV || 'development';
+gulp.task('server:watch', () => {
   const stream = nodemon({
-    script: `run.js`,
+    script: `app/server/run.js`,
     ext: 'js json',
     watch: ['app/server', 'internals'],
     stdout: false,
     readable: false,
-    tasks: ['webpack:server'],
     debug: true,
     env: {
-      DEBUG: process.env.DEBUG || 'ing:*',
-      NODE_ENV: env,
+      NODE_ENV: 'production',
     },
   }).on('readable', function() {
     let bunyan;
@@ -58,25 +45,6 @@ gulp.task('server:watch', ['webpack:development'], () => {
       stream.emit('restart', 10);
     })
 });
-
-function webpackProcess(done, config) {
-  webpack(config).run((err, stats) => {
-    if (err) {
-      throw new gutil.PluginError('webpack', err);
-    }
-    gutil.log('[webpack]', stats.toString({
-      hash: false,
-      colors: true,
-      cached: false,
-      chunks: false,
-      modules: false,
-      version: false,
-      children: false,
-      cachedAssets: false,
-    }));
-    done();
-  });
-}
 
 gulp.task('dict', () => {
   const API_ENV = process.env.API_ENV;
