df5fb858c4b1b164edf068f13e12559e245b708b
zhouxiao <zhouxiao@36kr.com>
2017-03-14 11:59:14 +0800

    fix: 部署脚本修改，03 04机器

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -14,11 +14,11 @@ rsync -rvltOD ./build/client/* ali-rong-api-02:$cdnstr
 
 ###### 业务逻辑
 pathstr="/data/work/frontend/prod/youyu/dist"
-if [ $1 = 'site1' ]; then
-    ssh ali-rong-proxy-01 "mkdir -p $pathstr"
+if [ $1 = 'site3' ]; then
+    ssh ali-rong-proxy-03 "mkdir -p $pathstr"
     rsync -rvltOD ./build/client/* ali-rong-proxy-01:$pathstr
-elif [ $1 = 'site2' ]; then
-    ssh ali-rong-proxy-02 "mkdir -p $pathstr"
+elif [ $1 = 'site4' ]; then
+    ssh ali-rong-proxy-04 "mkdir -p $pathstr"
     rsync -rvltOD ./build/client/* ali-rong-proxy-02:$pathstr
 elif [ $1 = 'sim' ]; then
     ssh ali-rong-sim-01 "mkdir -p $pathstr"
