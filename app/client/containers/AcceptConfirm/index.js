228cbcccace2ed8a42c9a3e0d3af4c203bf5b9d3
小包 <luxinyue@36kr.com>
2017-03-15 17:51:39 +0800

    feat: 下单确认链接样式

--- app/client/containers/AcceptConfirm/index.js
+++ app/client/containers/AcceptConfirm/index.js
@@ -98,7 +98,7 @@ export class AcceptConfirm extends PureComponent {
                   <input type="radio" name="memberType" onChange={this.agree.bind(this)} value="agree" id="agree" />
                 </div>
                 <label htmlFor="agree">
-                  同意：<a href="/protocol/deposit" target="_blank">《保证金管理制度》</a>
+                  同意：<a className="link-a" href="/protocol/deposit" target="_blank">《保证金管理制度》</a>
                 </label>
               </td>
             </tr>
